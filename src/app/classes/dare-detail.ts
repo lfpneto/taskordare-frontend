6edc5fdfdc4fa50aa71358a151825cd2c4688294
Filipe Cerqueira <imfilcarpt@gmail.com>
2022-12-12 23:49:47 +0000

    class of dareDetail updated to the new structure

--- src/app/classes/dare-detail.ts
+++ src/app/classes/dare-detail.ts
@@ -2,9 +2,10 @@ export class DareDetail {
     // id 	owner_id 	group_id 	task_name 	description 	points 	deadline 
     id: number = 0;
     ownerId: number = 0;
-    groupId: number = 0;
     dareName: string = "";
     description: string = "";
     points: number = 0;
-    deadline: string = "2022-12-01T12:25:45.973Z";//"2022-12-01T12:25:45.973Z"
+    deadline: string = "2022-12-01T12:25:45.973Z";//"2022-12-01T12:25:45.973Z";
+    status: number = 0;
+    url: string = "";
 }
