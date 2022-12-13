cb8c49f1c48a74ddd253f71ff64ddfac0498ae2a
Filipe Cerqueira <imfilcarpt@gmail.com>
2022-12-13 23:41:01 +0000

    task-service add

---
+++ src/app/classes/task-detail.ts
@@ -0,0 +1,12 @@
+export class TaskDetail {
+    id: number = 0;
+    ownerId: number = 0;
+    challengerId: number = 0;
+    groupId: number = 0;
+    taskName: string = "";
+    description: string = "";
+    points: number = 0;
+    deadline: string = "2022-12-01T12:25:45.973Z";//"2022-12-01T12:25:45.973Z";
+    status: number = 0;
+    url: string = "";
+}
--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
+import { TaskDetail } from '../classes/task-detail';
 
 @Injectable({
   providedIn: 'root'
@@ -10,5 +11,42 @@ export class TaskService {
   private baseUrl = environment.apiURL + 'tasks';
 
   constructor(private http: HttpClient) { }
+
+    //POST METHODS
+    createTask(TaskDetail: TaskDetail) {
+      console.log('createTask');
+      let url = this.baseUrl + '/create';
+      return this.http.post(url, TaskDetail);
+    }
+  
+    updateTask(TaskDetail: TaskDetail) {
+      //console.log("updateTask");
+      let url = this.baseUrl + '/update';
+      console.log(url);
+      return this.http.post(url, TaskDetail);
+    }
+  
+    deleteTask(TaskId: number) {
+      console.log('deleteTask');
+      let url = this.baseUrl + '/delete';
+      return this.http.post(url, TaskId);
+    }
+  
+    //GET METHODS
+    getAllTasks() {
+      let url = this.baseUrl + '/Tasks';
+      console.log('getAllTasks');
+      return this.http.get(url);
+    }
+  
+    getTaskbyId(TaskId: number) {
+      let url = this.baseUrl + '/Task/';
+      return this.http.get(url + TaskId);
+    }
+  
+    getUserTasks(userId: any) {
+      let url = this.baseUrl + '/Task/';
+      return this.http.get(url + '/' + userId);
+    }
   
 }
