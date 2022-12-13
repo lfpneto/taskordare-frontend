0e95e46b5a9a9dbadfbec8ceff6b25acac2aa742
Filipe Cerqueira <imfilcarpt@gmail.com>
2022-12-13 13:58:49 +0000

    Image upload on dares working

--- src/app/services/dare.service.ts
+++ src/app/services/dare.service.ts
@@ -8,55 +8,62 @@ import {
 import { Observable } from 'rxjs';
 import { DareDetail } from '../classes/dare-detail';
 import { environment } from 'src/environments/environment';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DareService {
   private baseUrl = environment.apiURL + 'dares';
+  private baseUserProfileUrl = environment.apiURL + 'user-profile';
+  private imageAPIUploader =
+    'https://freeimage.host/api/1/upload?key=6d207e02198a847aa98d0a2a901485a5';
 
-
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private _sanitizer: DomSanitizer) {}
 
   //POST METHODS
-  createDare(dareDetail: DareDetail){
-    console.log("createDare");
+  createDare(dareDetail: DareDetail) {
+    console.log('createDare');
     let url = this.baseUrl + '/create';
     return this.http.post(url, dareDetail);
   }
 
-  updateDare(dareDetail: DareDetail){
-    console.log("updateDare");
+  updateDare(dareDetail: DareDetail) {
+    //console.log("updateDare");
     let url = this.baseUrl + '/update';
+    console.log(url);
     return this.http.post(url, dareDetail);
   }
 
-  deleteDare(dareId: number){
-    console.log("deleteDare");
+  deleteDare(dareId: number) {
+    console.log('deleteDare');
     let url = this.baseUrl + '/delete';
     return this.http.post(url, dareId);
   }
 
   //GET METHODS
-  getAllDares(){
+  getAllDares() {
     let url = this.baseUrl + '/dares';
-    console.log("getAllDares")
+    console.log('getAllDares');
     return this.http.get(url);
   }
 
-  getDareInformation(dareId: number){
+  getDareInformation(dareId: number) {
     let url = this.baseUrl + '/dare/';
-    console.log("getDareInformation")
+    console.log('getDareInformation');
     return this.http.get(url + dareId);
   }
 
-  getUserDares(userName: any){
-    let url = this.baseUrl + '/endpointName?';
-    console.log("getUserDares")
-    return this.http.get(url + '{userName}?userName=' + userName);
+  getUserDares(userId: any) {
+    let url = this.baseUserProfileUrl + '/dares';
+    //console.log("getUserDares")
+    return this.http.get(url + '/' + userId);
   }
 
-
-
-
+  upload(file: any): Observable<any> {
+    const formData = new FormData(); // Store form name as "file" with file data
+    formData.append('source', file, file.name); // Make http post request over api // with formData as req
+    console.log(formData);
+    return this.http.post(this.imageAPIUploader, formData)
+  }
 }
--- src/app/user-dares/user-dares.component.ts
+++ src/app/user-dares/user-dares.component.ts
@@ -7,40 +7,45 @@ import { NavigationEnd, Router } from '@angular/router';
 @Component({
   selector: 'app-user-dares',
   templateUrl: './user-dares.component.html',
-  styleUrls: ['./user-dares.component.scss']
+  styleUrls: ['./user-dares.component.scss'],
 })
 export class UserDaresComponent implements OnInit {
   faUpload = faUpload;
   htmlToAdd: any;
+  isChecked: boolean = false;
+
+  showUploadImg: boolean = false;
+
+  //file uploader
+  url: any;
+  msg = '';
 
   private dareDetail = new DareDetail();
 
-  constructor(
-    private router: Router,
-    private dareService: DareService,
-  ) {
+  constructor(private router: Router, private dareService: DareService) {
     this.router.events.subscribe((ev) => {
-      if (ev instanceof NavigationEnd) { 
+      if (ev instanceof NavigationEnd) {
         /* Your code goes here on every router change */
-        if(ev.url == "/userdares"){
-          let result:any;
+        if (ev.url == '/userdares') {
+          let result: any;
           //todo: change to getUserDares according to token
-          this.dareService.getAllDares().subscribe(
+          let userId = localStorage.getItem('id');
+          this.dareService.getUserDares(userId).subscribe(
             (response) => {
               result = response;
-              if (result.status == "OK") {
+              if (result.status == 'OK') {
                 var i = 0;
-                result.data.Dares.forEach((element: { deadline: any; }) => {
-                  result.data.Dares[i].daysleft = this.calculateDiff(element.deadline);
+                result.data.Users.forEach((element: { deadline: any }) => {
+                  result.data.Users[i].daysleft = this.calculateDiff(
+                    element.deadline
+                  );
                   i = i + 1;
                 });
 
-                this.htmlToAdd = result.data.Dares;
+                this.htmlToAdd = result.data.Users;
               }
               if (result == -1) {
-                alert(
-                  'error'
-                );
+                alert('error');
               }
             },
             (error) => {
@@ -48,32 +53,124 @@ export class UserDaresComponent implements OnInit {
             }
           );
 
-          console.log("onInit  User-groups - end");
+          console.log('onInit  User-groups - end');
         }
       }
     });
-
   }
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  showDareInfo(dareId: number) {}
+
+  changeStatusChecked(dare: DareDetail) {
+    if (dare.url != '') {
+      if (dare.status == 1) {
+        dare.status = 0;
+      } else {
+        dare.status = 1;
+      }
+      this.updateDareResponse(dare);
+    }
   }
 
-  showDareInfo(dareId: number){
-
+  updateDareResponse(dare: DareDetail) {
+    let result: any;
+    this.dareService.updateDare(dare).subscribe(
+      (response) => {
+        result = response;
+        if (result.status == 'OK') {
+          alert('Updated');
+        }
+        if (result == -1) {
+          alert('error');
+        }
+      },
+      (error) => {
+        console.log('Errors (CORS?) - ' + JSON.stringify(error));
+      }
+    );
   }
 
-  calculateDiff(dateSent: any){
+  calculateDiff(dateSent: any) {
     let currentDate = new Date();
     dateSent = new Date(dateSent);
-    var difference = Math.floor((Date.UTC(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate()) - Date.UTC(dateSent.getFullYear(), dateSent.getMonth(), dateSent.getDate()) ) /(1000 * 60 * 60 * 24));
+    var difference = Math.floor(
+      (Date.UTC(
+        currentDate.getFullYear(),
+        currentDate.getMonth(),
+        currentDate.getDate()
+      ) -
+        Date.UTC(
+          dateSent.getFullYear(),
+          dateSent.getMonth(),
+          dateSent.getDate()
+        )) /
+        (1000 * 60 * 60 * 24)
+    );
     // return -difference;
-    if(difference > 0){
-      return "Days overdue: " + -difference;
-    }else{
-      return "Days left: " + -difference;
+    if (difference > 0) {
+      return 'Days overdue: ' + -difference;
+    } else {
+      return 'Days left: ' + -difference;
     }
-}
+  }
 
-  
+  // selectFile(event: any, dare: DareDetail) {
+  //     //Angular 11, for stricter type
+  //     if (!event.target.files[0] || event.target.files[0].length == 0) {
+  //       this.msg = 'You must select an image';
+  //       return;
+  //     }
 
+  //     var mimeType = event.target.files[0].type;
+
+  //     if (mimeType.match(/image\/*/) == null) {
+  //       this.msg = 'Only images are supported';
+  //       return;
+  //     }
+
+  //     var reader = new FileReader();
+  //     reader.readAsDataURL(event.target.files[0]);
+
+  //     reader.onload = (_event) => {
+  //       this.msg = '';
+  //       this.url = reader.result;
+  //     };
+
+  // 	//this.imageUpload(event.target.files[0].name, dare);
+  // 	let result: any;
+  // 	this.dareService.uploadImagePost(event.target.files[0].name).subscribe(
+  // 		(response) => {
+  // 		  result = response;
+  // 		  console.log(result);
+  // 		  if (result.status_code == 200) {
+
+  // 		  } else {
+  // 			alert('error');
+  // 		  }
+  // 		  if (result == -1) {
+  // 			alert('error');
+  // 		  }
+  // 		},
+  // 		(error) => {
+  // 		  console.log('Errors (CORS?) - ' + JSON.stringify(error));
+  // 		}
+  // 	  );
+  //   }
+
+  // OnClick of button Upload
+  onUpload(event: any, dare: DareDetail) {
+    console.log(event);
+    if (event.target.files[0]) {
+      console.log('entrei');
+      this.dareService.upload(event.target.files[0]).subscribe((event: any) => {
+        console.log(event);
+        if (typeof event === 'object') {
+			dare.url = event.image.display_url;
+			this.updateDareResponse(dare);
+        }
+      });
+    }
+  }
 }
